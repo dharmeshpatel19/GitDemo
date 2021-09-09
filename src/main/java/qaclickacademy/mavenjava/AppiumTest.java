e71642143b08a9db74a7046daa1ef8fc0a87fab7
dharmesh <dharmesh.s.patel19@gmail.com>
2021-09-09 14:59:04 -0500

    First Commit

---
+++ src/main/java/qaclickacademy/mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAnroid()
+	{
+		System.out.println("NativeAppAnroid");
+	}
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+
+	}
+
+}
